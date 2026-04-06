3ae91db307156d2dccb86525f9284c034c6b7f35
Marco Carminati <marco@carminati.org>
2026-04-06 23:36:02 +0200

    fix CORS: expose custom headers per feedback export iPratico (X-Added-Missing, X-Updated-Qty, ecc.)

--- main.py
+++ main.py
@@ -93,6 +93,14 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=[
+        "Content-Disposition",
+        "X-Updated-Qty",
+        "X-Updated-Price",
+        "X-Updated-Name",
+        "X-Total-Matched",
+        "X-Added-Missing",
+    ],
 )
 
 
